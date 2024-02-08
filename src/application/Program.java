948eb2cb026a839cfe31abfa214f32a2794d6d0b
Eric Feitosa <eric_feitosa13@hotmail.com>
2024-02-08 18:48:00 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
 
 	
 		
@@ -39,13 +43,19 @@ public class Program {
 		System.out.println("Inserted! New id = " + newSeller.getId());
 
 		System.out.println("\n=== TEST 5 : seller update ===");
-		seller = sellerDao.findById(1);
+		seller = sellerDao.findById(2);
 		seller.setName("Shayanie");
 		seller.setEmail("Shayanie@gmail.com");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		//System.out.println("\n=== TEST 6 : seller DeleteById ===");
+		//System.out.println("Enter Id for delete: ");
+		//int id = sc.nextInt();
+		//sellerDao.deleteById(id);
+		//System.out.println("Delete completed!");
 
-
+		sc.close();
 	}
 
 }
---
+++ src/snippet/Snippet.java
@@ -0,0 +1,9 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		System.out.println("\n=== TEST 5 : seller update ===");
+		
+	}
+}
+
