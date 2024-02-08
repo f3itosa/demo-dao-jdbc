c280ca2820a3246d2748bbb54eccb6ba4068433a
Eric Feitosa <eric_feitosa13@hotmail.com>
2024-02-08 18:30:13 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,14 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 
+		System.out.println("\n=== TEST 5 : seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Shayanie");
+		seller.setEmail("Shayanie@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+
+
 	}
 
 }
